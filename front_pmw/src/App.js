85aadb18bb15d6be3b189b9b5291b69520a68045
JhonEddi <31699155+JhonEddi@users.noreply.github.com>
2023-03-08 21:07:40 -0500

    Organize

--- front_pmw/src/App.js
+++ front_pmw/src/App.js
@@ -2,8 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
-import { Projects } from './components/Projects';
 import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
