c74bdda25a1a84f92813b3ca3b7dfca044eb4164
SantiYami <davidgarces.98@gmail.com>
2023-03-16 22:44:01 -0500

    Animations
    Finalizado

--- front_pmw/src/components/Banner.js
+++ front_pmw/src/components/Banner.js
@@ -2,11 +2,13 @@ import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/Header.svg";
+import "animate.css";
+import TranckVisibility from "react-on-screen";
 
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = [ "Web", "de Software", "de Aplicaciones Moviles" ];
+    const toRotate = ["Web", "de Software", "de Aplicaciones Moviles"];
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 - Math.random() * 100);
     const period = 2000;
@@ -26,7 +28,7 @@ export const Banner = () => {
         setText(updatedText);
 
         if (isDeleting) {
-            setDelta(prevDelta => prevDelta / 2)            
+            setDelta(prevDelta => prevDelta / 2)
         }
         if (!isDeleting && updatedText === fullText) {
             setIsDeleting(true);
@@ -43,13 +45,19 @@ export const Banner = () => {
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
-                        <span className="tagline">Bienvenidos a su nuevo futuro</span>
-                        <h1>{`Te capacitamos en el desarrollo `}<span className="wrap">{text}</span></h1>
-                        <p>TechMight es una plataforma en linea en la cual podras instruirte y encontrar la oportunidad de impulsar tus oportunidades en el mundo laborar, para ti mismo como para tus empleados en el mundo de la tecnologia y en el desarrollo de software en Colombia. No dudes en contactarnos.</p>
-                        <button onClick={() => console.log('connect')}>Conocenos<ArrowRightCircle size={25}/></button>
+                        <TranckVisibility>
+                            {({ isVisible }) =>
+                                <div className={isVisible ? "animate__animated animate__bounce" : ""}>
+                                    <span className="tagline">Bienvenidos a su nuevo futuro</span>
+                                    <h1>{`Te capacitamos en el desarrollo `}<span className="wrap">{text}</span></h1>
+                                    <p>TechMight es una plataforma en linea en la cual podras instruirte y encontrar la oportunidad de impulsar tus oportunidades en el mundo laborar, para ti mismo como para tus empleados en el mundo de la tecnologia y en el desarrollo de software en Colombia. No dudes en contactarnos.</p>
+                                    <button onClick={() => console.log('connect')}>Conocenos<ArrowRightCircle size={25} /></button>
+                                </div>
+                            }
+                        </TranckVisibility>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
-                        <img src={headerImg} alt="Headder Img"/>
+                        <img src={headerImg} alt="Headder Img" />
                     </Col>
                 </Row>
             </Container>
